86be86971c602fe3c9be21545cb2fc3e24a9a12c
Locco0103 <toshihiro.l.kojima@gmail.com>
2017-10-31 11:09:02 +0900

    learn-client ‘gulpfile.js’ , ‘package.json’ changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
+var plumber = require('gulp-plumber');
 
 // gulp.task('default', function() {  
 //     console.log("デフォルトタスクだよー");
@@ -9,16 +10,22 @@ gulp.task('hoge', function() {
     console.log("ほげタスクだよー");
 });
 
+// ファイルの生成
 gulp.task('build', function() {  
-  return gulp.src(['*.pug'])
+  return gulp.src(['lp/**/*.pug'])
+  // return gulp.src(['lp/**/*.pug','books/**/**/*.pug'])
+    .pipe(plumber())
     .pipe(pug({
       pretty: true
     }))
-    .pipe(gulp.dest('./'));
+    // ./ -> ./lp/ に変えてみます
+    .pipe(gulp.dest("./lp/"));
 });
 
 gulp.task('default', ['build']);
 
 gulp.task('watch', function(){
-  gulp.watch('*.pug' ,['build']);
+  gulp.watch('lp/**/*.pug' ,['build']);
+// 10.27 書いてみるが動かず
+  // gulp.watch('books/**/**/*.pug' ,['build']);
 });
