50f7d953353ed4082b55d1b1839c2424cbeb4f1f
Locco0103 <toshihiro.l.kojima@gmail.com>
2017-11-06 16:42:07 +0900

    lp trace ‘hitokara’ responsive done

--- lp/hitokara/js/main.js
+++ lp/hitokara/js/main.js
@@ -4,26 +4,23 @@ $(function(){
   $('.menuPlus').parents('li').next('ul').hide();
 
   $('.menuPlus').click(function(){
-    // if( $(this).hasClass('active') ){
-    //   $(this).parents('li').next('ul').fadeOut(100);
-    //   $(this).addClass('fa-plus').removeClass('active fa-minus');                
-    // } else {
-    //   $(this).parents('li').next('ul').fadeIn(100);
-    //   $(this).addClass('active fa-minus').removeClass('fa-plus');                
-    // }
-// もしや全部 toggle でできるのでは？
     $(this).parents('li').next('ul').fadeToggle(100);
     $(this).toggleClass('fa-plus fa-minus');
     $(this).parent().siblings().toggleClass('color1');
   });
 
 // ハンバーガーメニューの役割
-  $('#sMenu').hide();
-
+  $('#smMenu').hide();
   $('#ham').click(function(){
-    $('#sMenu').slideToggle(500);
+    $('#smMenu').not(":animated").slideToggle(300);
   });
 
-
+// lMenu 出力
+  $('.subMenu').hide(); 
+  $('#lMenu div').hover(function(){
+    $(this).children('.subMenu').show();
+  },function(){
+    $(this).children('.subMenu').hide();
+  });
 
 });
